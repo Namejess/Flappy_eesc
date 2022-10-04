78ec13bd85c49f9aa9df2af102cfbfb2adfcd57d
Namejess <jessy_drouin@protonmail.com>
2022-10-04 10:53:32 +0200

    Initial commit

--- src/main/java/edu/jdrouin/eesc/Oiseau.java
+++ src/main/java/edu/jdrouin/eesc/Oiseau.java
@@ -6,6 +6,7 @@ public class Oiseau extends Carre{
 
     protected int vitesseVertical;
 
+
 //////////////////////////////////////
 //           CONSTRUCT              //
 //////////////////////////////////////
